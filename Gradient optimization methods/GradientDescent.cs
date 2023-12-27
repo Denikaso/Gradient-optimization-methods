eb2180cbb90bd31d2476550c9f89e8f3bb3d212b
Denikaso <daniil.tata@mail.ru>
2023-12-27 17:49:57 +0300

    Добавьте файлы проекта.

---
+++ Gradient optimization methods/GradientDescent.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradient_optimization_methods
+{
+    internal class GradientDescentMethod
+    {        
+        public delegate double CostFunction(Vector2 x);
+        public delegate Vector2 GradientFunction(Vector2 x);
+
+        static public Vector2 BacktrackingLineSearch(CostFunction costFunction, GradientFunction gradient, Vector2 startingPoint, float epsilon, float userAlpha, out bool finish, bool isMinimization = true)
+        {
+            finish = true;
+            Vector2 x = startingPoint;
+            float alpha = userAlpha; // Используем значение, введенное пользователем
+            float beta = 0.5f; // Коэффициент уменьшения шага
+            int iteration = 0;
+
+            while (gradient(x).Length() > epsilon)
+            {
+                iteration++;                
+                // Вычисляем производные в текущей точке
+                Vector2 grad = gradient(x);
+
+                // Вычисляем модуль градиента
+                float gradMagnitude = grad.Length();
+
+                // Проверка, чтобы избежать деления на ноль
+                if (gradMagnitude > float.Epsilon)
+                {                    
+                    // Вычисляем новую точку с учетом модуля градиента
+                    Vector2 newX = x - alpha * (grad / gradMagnitude);
+
+                    // Вычисляем значение функции в новых точках
+                    double newCost = costFunction(newX);
+                    double oldCost = costFunction(x);
+
+                    if (iteration == 10000)
+                    {
+                        Console.WriteLine("Прошло 10к итераций, ответ не получен. Текущие значения:");
+                        Console.WriteLine($"Минимум функции: {newCost}");
+                        Console.WriteLine($"Точка минимума: {x}");
+                        finish = false;
+                        break;
+                    }
+
+                    // Условие Вольфа для минимизации
+                    while (isMinimization ? (newCost >= oldCost + beta * alpha * Vector2.Dot(grad, newX - x)) : (newCost <= oldCost + beta * alpha * Vector2.Dot(grad, newX - x)))
+                    {
+                        alpha *= 0.5f;
+
+                        // Пересчитываем новую точку
+                        newX = x - alpha * (grad / gradMagnitude);
+
+                        // Пересчитываем значение функции в новых точках
+                        newCost = costFunction(newX);
+                    }
+
+                    x = newX;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return x;
+        }
+
+
+    }
+}
---
+++ Gradient optimization methods/Program.cs
@@ -0,0 +1,52 @@
+﻿using Gradient_optimization_methods;
+using System.Numerics;
+
+static double Function1(Vector2 x)
+{
+    return 5 - Math.Pow(x.X - 4, 2) - Math.Pow(x.Y - 5, 2);
+}
+
+static Vector2 Gradient1(Vector2 x)
+{
+    double df_dx1 = 8 - 2 * x.X;
+    double df_dx2 = -2 * (x.Y - 5);
+    return new Vector2((float)df_dx1, (float)df_dx2);
+}
+
+static double Function2(Vector2 x)
+{
+    return 2 * Math.Pow(x.X, 2) - x.X * x.Y + Math.Pow(x.Y, 2) - x.X - x.Y + 1;
+}
+
+static Vector2 Gradient2(Vector2 x)
+{
+    double df_dx1 = 4 * x.X - x.Y - 1;
+    double df_dx2 = -x.X + 2 * x.Y - 1;
+    return new Vector2((float)df_dx1, (float)df_dx2);
+}
+
+
+float initialX1, initialX2, alpha, epsilon;
+
+Console.Write("Введите начальное значение x1: ");
+initialX1 = float.Parse(Console.ReadLine());
+
+Console.Write("Введите начальное значение x2: ");
+initialX2 = float.Parse(Console.ReadLine());
+
+Console.Write("Введите шаг: ");
+alpha = float.Parse(Console.ReadLine());
+
+Console.Write("Введите точность: ");
+epsilon = float.Parse(Console.ReadLine());
+
+Vector2 initialPoint = new Vector2(initialX1, initialX2);
+bool finish;
+Vector2 minPointBacktracking = GradientDescentMethod.BacktrackingLineSearch(Function1, Gradient1, initialPoint, epsilon, alpha, out finish);
+
+if(finish)
+{
+    Console.WriteLine("\nМетод градиента с дроблением шага:");
+    Console.WriteLine($"Минимум функции: {Function2(minPointBacktracking)}");
+    Console.WriteLine($"Точка минимума: {minPointBacktracking}");
+}
